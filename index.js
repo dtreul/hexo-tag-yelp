c1b5c96b2e3a33379f854344c8ef792d47c5fcbd
Derrick Treul <treul004@umn.edu>
2016-10-05 01:44:11 -0500

    I hat ethis

--- index.js
+++ index.js
@@ -6,39 +6,79 @@
 
 // var $ = require('jQuery');
 // var http = require('http');
-var request = require('request');
+// var Promise = require('bluebird');
+// var request = require('request');
+// var async = require('async');
+var rp = require('request-promise');
+// var config = hexo.config.yelp || {};
+
+// var Yelp = require('yelp');
+// var key = config.key
+
+// var promiseRequest = function (bus_id) {
+//   return new Promise(function (resolve, reject) {
+
+//   })
+// }
+
 
 hexo.extend.tag.register('yelp', function (args) {
-  var base_url = 'https://api.yelp.com/v3/businesses/';
   var business_id = args[0];
+  var base_url = 'https://api.yelp.com/v3/businesses/';
   // var business_id = 'fun';
   var config = hexo.config.yelp || '';
-  var key = config.key;
-  // var key = 'a';
-  if (key == '') {
+  if (config == '') {
     return;
   }
 
+  // console.log(config);
+
+  // var key = 'a';
+  // var business_data = {};
+  // var yelp = new Yelp({
+  //   consumer_key: config.consumer_key,
+  //   consumer_secret: config.consumer_secret,
+  //   token: config.token,
+  //   token_secret: config.token_secret
+  // })
+
+  // return yelp.business(business_id).then(function (data) { Promise.resolve(data);});
 
   var business_url = base_url + business_id;
-  var business_data;
 
   var options = {
     url: business_url,
     headers: {
-      "Authorization": "Bearer " + key
-    }
+      "Authorization": "Bearer " + config.key
+    },
+    json: true
   };
 
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      business_data = JSON.parse(body);
-    } else {
-      business_data = {};
-    }
-  }
-  request(options, callback);
 
-  return business_data;
 
-});
+  var p =  rp(options)
+    .then(function (r) {
+       return Promise.resolve(r);
+    });
+
+  // return p;
+  // var business_data;
+  // function callback(error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     // console.log(response);
+  //     // console.log(body);
+  //     console.log(body);
+  //     return body;
+  //     // console.log(business_data)
+  //   } else {
+  //     return {};
+  //   }
+  // }
+  // return request(options, callback);
+  // console.log(business_data);
+  // console.log(business_data.name);
+  // return business_data;
+
+}, {async: true});
+
+
