0a690ca4f5e15a7c2852221b3247b1c9f8771902
Derrick Treul <treul004@umn.edu>
2016-10-04 21:48:30 -0500

    finished?

--- index.js
+++ index.js
@@ -4,26 +4,40 @@
  *
 */
 
-var $ = require('jQuery');
+// var $ = require('jQuery');
+// var http = require('http');
+var request = require('request');
 
 hexo.extend.tag.register('yelp', function (args) {
   var base_url = 'https://api.yelp.com/v3/businesses/';
   var business_id = args[0];
+  // var business_id = 'fun';
   var key = hexo.config.yelp || ''
-  
+  // var key = 'a';
   if (key == '') {
     return;
   }
-  var url = base_url + business_id;
+
+
+  var business_url = base_url + business_id;
   var business_data;
-  $.get(
-    url,
-    {"Authorization": "Bearer " + key},
-    function(data) {
-      business_data = data;
-    },
-    "json"
-    );
+
+  var options = {
+    url: business_url,
+    headers: {
+      "Authorization": "Bearer " + key
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      business_data = JSON.parse(body);
+    } else {
+      business_data = {};
+    }
+  }
+  request(options, callback);
+  
   return business_data;
 
 });
